8a26dc2f92287092e233cf1a7efb500300eddc05
Pekkzzz <tillocas3@gmail.com>
2026-03-12 11:17:25 -0500

    Reglas
    Coloque las reglas en el layout

--- ProyectoOodle/src/main/java/com/example/proyectooodle/HelloController.java
+++ ProyectoOodle/src/main/java/com/example/proyectooodle/HelloController.java
@@ -2,6 +2,8 @@ package com.example.proyectooodle;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.text.TextAlignment;
+
 
 public class HelloController {
     @FXML
@@ -9,6 +11,16 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("REGLAS" +
+                " \n Utilice los números del 1 al 9 sólo una vez, o del 1 al 12 \n" +
+                "Haga click en el cuadro de respuestas rojo para verificar la solución \n" +
+                "Verde = ¡El número está en la posición correcta!"+
+                "\nAmarillo = Número de posición incorrecta"+
+                 "\nGris = Número no presente e la ecuación"+
+                "\nTienes un máximo de seis intentos");
+
+        welcomeText.setTextAlignment(TextAlignment.CENTER);
+
+
     }
 }
