179267180b13f7f257386743e21c5b53b236cbc8
Pekkzzz <tillocas3@gmail.com>
2026-03-12 11:41:06 -0500

    cambio de rango boton

--- ProyectoOodle/src/main/java/com/example/proyectooodle/HelloController.java
+++ ProyectoOodle/src/main/java/com/example/proyectooodle/HelloController.java
@@ -1,26 +1,49 @@
 package com.example.proyectooodle;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.text.TextAlignment;
-
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button btnCambiarModo;
+
+    private boolean modoActual = false;
+
+    @FXML
     protected void onHelloButtonClick() {
+        mostrarReglas();
+    }
+
+    @FXML
+    protected void onCambiarModo() {
+
+        modoActual = !modoActual;
+
+        if (modoActual) {
+            btnCambiarModo.setText("Cambiar a modo 1-9");
+        } else {
+            btnCambiarModo.setText("Cambiar a modo 1-12");
+        }
+
+        mostrarReglas();
+    }
+
+    private void mostrarReglas() {
+        String numeros = modoActual ? "1 al 9" : "1 al 12";
+
         welcomeText.setText("REGLAS" +
-                " \n Utilice los números del 1 al 9 sólo una vez, o del 1 al 12 \n" +
-                "Haga click en el cuadro de respuestas rojo para verificar la solución \n" +
-                "Verde = ¡El número está en la posición correcta!"+
-                "\nAmarillo = Número de posición incorrecta"+
-                 "\nGris = Número no presente e la ecuación"+
+                "\nUtilice los números del " + numeros + " sólo una vez\n" +
+                "Haga click en el cuadro de respuestas rojo para verificar la solución\n" +
+                "Verde = ¡El número está en la posición correcta!" +
+                "\nAmarillo = Número de posición incorrecta" +
+                "\nGris = Número no presente en la ecuación" +
                 "\nTienes un máximo de seis intentos");
 
         welcomeText.setTextAlignment(TextAlignment.CENTER);
-
-
     }
 }
